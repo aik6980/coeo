44383f0fd7d46a0be67e4bfbb819ddc739725caf
aik6980 <aik6980>
2019-06-27 09:04:42 +0100

    using copyplugin to copy vendor files

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const PATH = require('path');
 const WEBPACK = require('webpack');
+const COPY_PLUGIN = require('copy-webpack-plugin');
 
 module.exports = {
     entry: {
@@ -19,5 +20,11 @@ module.exports = {
     output: {
         filename: '[name].js',
         path: __dirname + '/public/js'
-    }
+    },
+    plugins: [
+        new COPY_PLUGIN([
+            { from: './src/js/vendor', to: __dirname + '/public/js/vendor' },
+            { from: './res', to: __dirname + '/public/game_assets' },
+        ])
+    ]
 };
